64be474d4cc40eb23fc50574e2c08c09f2910e0b
Paul Crooks <pgcrooks@gmail.com>
2019-12-08 17:03:06 +0000

    Add version number

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
+#include <aoc_config.h>
+
 #include "module.hpp"
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Advent of Code" << std::endl;
+    std::cout << "Advent of Code "
+        << advent_of_code_VERSION_MAJOR
+        << "."
+        << advent_of_code_VERSION_MINOR
+        << std::endl;
 
     advent::Module mod1("mod1", 1969);
     std::cout << "Fuel needed = " << mod1.calculate_fuel() << std::endl;
