d20abe2345fdd2ea873e6ffd1389be3d2cf64a74
Paul Crooks <pgcrooks@gmail.com>
2019-12-11 21:10:15 +0000

    Add param test
    Does not check return values yet, need to switch to pairs.

--- test/test_module.cpp
+++ test/test_module.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <gtest/gtest.h>
 
 #include <module.hpp>
@@ -9,5 +11,23 @@ TEST(TestModule, ConstructorReturnsModule)
 {
     advent::Module mod1("mod1", 1969);
 }
+
+
+class ModuleNumberTest : public ::testing::TestWithParam<int>
+{
+public:
+    ModuleNumberTest()
+    {}
+};
+
+TEST_P(ModuleNumberTest, Examples)
+{
+    Module mod("mod1", GetParam());
+    std::cout << mod.calculate_fuel() << std::endl;
+}
+
+INSTANTIATE_TEST_SUITE_P(ExampleValues,
+    ModuleNumberTest,
+    testing::Values(12, 14, 1969, 100756));
 
 }
