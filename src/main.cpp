3bddde654bccb6aef10816f4abbc17ce0eef33b4
Paul Crooks <pgcrooks@gmail.com>
2019-12-11 20:19:40 +0000

    Let it build for now

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 
 #include <aoc_config.h>
 
-#include "module.hpp"
-
 int main(int argc, char *argv[])
 {
     std::cout << "Advent of Code "
@@ -11,9 +9,6 @@ int main(int argc, char *argv[])
         << "."
         << advent_of_code_VERSION_MINOR
         << std::endl;
-
-    advent::Module mod1("mod1", 1969);
-    std::cout << "Fuel needed = " << mod1.calculate_fuel() << std::endl;
 
     return 0;
 }
